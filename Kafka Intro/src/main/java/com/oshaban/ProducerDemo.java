a246233c139c8e185c5a92ebb20e9a331752117e
omarshaban <omar@energywatch-inc.com>
2020-09-22 08:56:25 -0400

    Created basic Kafka Producer

--- Kafka Intro/src/main/java/com/oshaban/ProducerDemo.java
+++ Kafka Intro/src/main/java/com/oshaban/ProducerDemo.java
@@ -1,8 +1,38 @@
 package com.oshaban;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
+    private static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        // Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create a Producer Record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
+
+        // Send data -- asynchronous
+        producer.send(record);
+
+        // Flush data
+        producer.flush();
+
+        // Flush and close producer
+        producer.close();
+
     }
 }
