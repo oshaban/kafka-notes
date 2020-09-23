d9570c6e7debd5df2beec3bcb37c3624c3940f79
omarshaban <omar@energywatch-inc.com>
2020-09-22 21:17:28 -0400

    Added example Consumer demo

---
+++ Kafka Intro/src/main/java/com/oshaban/ConsumerDemo.java
@@ -0,0 +1,50 @@
+package com.oshaban;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class);
+
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+    private static final String GROUP_ID = "my-application";
+    private static final String TOPIC = "first_topic";
+
+    public static void main(String[] args) {
+
+        // Create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // Subscribe our consumer to our topic(s)
+        consumer.subscribe(Collections.singleton(TOPIC));
+
+        // Poll for new data
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100L));
+
+            for (ConsumerRecord<String, String> record: records) {
+                log.info("Key: {}, Value: {}", record.key(), record.value());
+                log.info("Partition: {}, Offset: {}", record.partition(), record.offset());
+            }
+        }
+
+    }
+}
